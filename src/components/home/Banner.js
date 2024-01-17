417d8247f55d90ae3920cbf0c46b06c389bcfc55
manfuji <manfuji@create-it.org>
2024-01-17 14:39:42 +0000

    changed the banner

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -1,86 +1,82 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 
 const Banner = () => {
   return (
-    <div>
-      <div className="relative pt-14">
-        <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-teal-300 to-[#05716a] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
-        </div>
-        <div className="py-24 sm:py-32">
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto max-w-2xl text-center">
+    <div className="relative ">
+      <div className="mx-auto max-w-7xl">
+        <div className="relative z-10 pt-14 lg:w-full lg:max-w-2xl">
+          <svg
+            className="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-white lg:block"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="0,0 90,0 50,100 0,100" />
+          </svg>
+
+          <div className="relative px-6 py-32 sm:py-40 lg:px-8 lg:py-56 lg:pr-0">
+            <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
+              <div className="hidden sm:mb-10 sm:flex">
+                <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                  We provide more than just data.
+                  <a
+                    href="#features"
+                    className="whitespace-nowrap font-semibold text-teal-600"
+                  >
+                    <span className="absolute inset-0" aria-hidden="true" />
+                    Read more <span aria-hidden="true">&rarr;</span>
+                  </a>
+                </div>
+              </div>
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                 What do we bring to you?
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Findata Bureau: A top-tier source for global corporate filings
-                and financial data, prioritizing professionalism, technology,
-                governance, market knowledge, and customer-centricity.
+                <span className="text-teal-600">Findata Bureau:</span> A
+                top-tier source for global corporate filings and financial data,
+                prioritizing professionalism, technology, governance, market
+                knowledge, and customer-centricity.
               </p>
-              <div className="mt-10 flex items-center justify-center gap-x-6">
+              <div className="mt-10 flex items-center gap-x-6">
                 <a
-                  href="/#pricing"
+                  href="#pricing"
                   className="rounded-md bg-teal-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
                 >
                   Get started
                 </a>
                 <a
-                  href="/#features"
+                  href="#features"
                   className="text-sm font-semibold leading-6 text-gray-900"
                 >
                   Learn more <span aria-hidden="true">→</span>
                 </a>
               </div>
             </div>
-            <div className="mt-16 flow-root sm:mt-24">
-              <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-                <div className="w-full rounded overflow-hidden shadow-lg">
-                  <video
-                    className="w-full h-[700px] object-cover"
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                    // width={2432}
-                    // height={1442}
-                  >
-                    <source
-                      src="/video/banner.mp4"
-                      type="video/mp4"
-                      className="w-full"
-                    />
-                    Your browser does not support the video tag.
-                  </video>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
-        <div
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true"
-        >
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
+      </div>
+      <div className="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+          <div className="w-full rounded overflow-hidden shadow-lg">
+            <video
+              className="w-full h-full object-cover"
+              autoPlay
+              loop
+              muted
+              playsInline
+            >
+              <source
+                src="/video/banner.mp4"
+                type="video/mp4"
+                className="w-full"
+              />
+              Your browser does not support the video tag.
+            </video>
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default Banner;
